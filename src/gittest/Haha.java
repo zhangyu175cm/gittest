704767e4c1f91dd10d2a62e961312ff2630c031a
zhangyu175cm <zhangyu175cm@yeah.net>
2019-08-17 16:18:51 +0800

    add final

---
+++ src/gittest/Haha.java
@@ -0,0 +1,14 @@
+package gittest;
+
+public class Haha {
+
+	public Haha() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("fads");
+	}
+
+}
